1c1a873a7247b8422ccb0b1cc90661d0b6671df8
bruno-rmf <bruno.salvatella@estudiant.enti.cat>
2025-12-11 09:59:21 +0100

    problemas houston

---
+++ AjedrezEsteSi/AjedrezEsteSi/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ANCHO 8
+#define ALTO 8
+
+char tablero[ALTO][ANCHO];
+bool gameOver = false;
+
+void imprimirTablero() {
+
+}
+
+
+int main() {
+    iniciarTablero();
+    imprimirTablero();
+    return 0;
+}
