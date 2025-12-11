b2b48f8260bd01638bfb45cd8dbf31bc4bff275c
bruno-rmf <bruno.salvatella@estudiant.enti.cat>
2025-12-11 10:38:58 +0100

    añadir turnos y dif funciones

--- AjedrezEsteSi/AjedrezEsteSi/main.cpp
+++ AjedrezEsteSi/AjedrezEsteSi/main.cpp
@@ -5,73 +5,83 @@
 char tablero[ALTO][ANCHO];
 bool gameOver = false;
 
-void imprimirTablero() {
-    std::cout << "  1 2 3 4 5 6 7 8 \n";
-    std::cout << "1 ";
-    tablero[1][1] = 't';
-    std::cout << "t ";
-    tablero[1][2] = 'h';
-    std::cout << "h ";
-    tablero[1][3] = 'b';
-    std::cout << "b ";
-    tablero[1][4] = 'k';
-    std::cout << "k ";
-    tablero[1][5] = 'q';
-    std::cout << "q ";
-    tablero[1][6] = 'b';
-    std::cout << "b ";
-    tablero[1][7] = 'h';
-    std::cout << "h ";
-    tablero[1][8] = 't';
-    std::cout << "t \n";
 
-    std::cout << "2 ";
-    for (int i = 0; i < ANCHO; i++)
-    {
-        tablero[2][i] = 'p';
-        std::cout << "p ";
+void inicializarTablero() {
+    
+    tablero[0][0] = 't';
+    tablero[0][1] = 'h';
+    tablero[0][2] = 'b';
+    tablero[0][3] = 'k';
+    tablero[0][4] = 'q';
+    tablero[0][5] = 'b';
+    tablero[0][6] = 'h';
+    tablero[0][7] = 't';
+
+    for (int i = 0; i < ANCHO; i++) {
+        tablero[1][i] = 'p';
     }
-    std::cout << "\n";
 
-    for (int i = 3; i < 7; i++)
+    for (int i = 2; i < 6; i++) 
     {
-        std::cout << i << " ";
-        for (int j = 0; j < ANCHO; j++)
+        for (int j = 0; j < ANCHO; j++) 
         {
-            tablero[i][j] == '*';
-            std::cout << "* ";
+            tablero[i][j] = '*';
         }
-        std::cout << "\n";
     }
-    std::cout << "7 ";
-    for (int i = 0; i < ANCHO; i++)
-    {
-        tablero[7][i] = 'P';
-        std::cout << "P ";
+   
+    for (int i = 0; i < ANCHO; i++) {
+        tablero[6][i] = 'P';
     }
-    std::cout << "\n";
-
-    std::cout << "8 ";
-    tablero[8][1] = 'T';
-    std::cout << "T ";
-    tablero[8][2] = 'H';
-    std::cout << "H ";
-    tablero[8][3] = 'B';
-    std::cout << "B ";
-    tablero[8][4] = 'K';
-    std::cout << "K ";
-    tablero[8][5] = 'Q';
-    std::cout << "Q ";
-    tablero[8][6] = 'B';
-    std::cout << "B ";
-    tablero[8][7] = 'H';
-    std::cout << "H ";
-    tablero[8][8] = 'T';
-    std::cout << "T \n";
+        
+    tablero[7][0] = 'T';
+    tablero[7][1] = 'H';
+    tablero[7][2] = 'B';
+    tablero[7][3] = 'K';
+    tablero[7][4] = 'Q';
+    tablero[7][5] = 'B';
+    tablero[7][6] = 'H';
+    tablero[7][7] = 'T';
 }
 
 
+void imprimirTablero() {
+    std::cout << "  1 2 3 4 5 6 7 8\n";
+
+    std::cout << "8 ";
+    for (int j = 0; j < ANCHO; j++)
+        std::cout << tablero[0][j] << " ";
+    std::cout << "\n";
+    
+    std::cout << "7 ";
+    for (int j = 0; j < ANCHO; j++)
+        std::cout << tablero[1][j] << " ";
+    std::cout << "\n";
+
+    for (int i = 2; i < 6; i++) {
+        std::cout << 8 - i << " ";
+        for (int j = 0; j < ANCHO; j++)
+            std::cout << tablero[i][j] << " ";
+        std::cout << "\n";
+    }
+   
+    std::cout << "2 ";
+    for (int j = 0; j < ANCHO; j++)
+        std::cout << tablero[6][j] << " ";
+    std::cout << "\n";
+    
+    std::cout << "1 ";
+    for (int j = 0; j < ANCHO; j++)
+        std::cout << tablero[7][j] << " ";
+    std::cout << "\n";
+}
+
 int main() {
-    imprimirTablero();
+    inicializarTablero();
+    while (!gameOver)
+    {
+        imprimirTablero();
+        system("cls");
+    }
+
     return 0;
 }
